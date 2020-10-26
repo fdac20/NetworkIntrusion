54eff33f2486f023f6a0e29ba34debb5eaa1f72e
Charlie <chazzo96@yahoo.com>
2020-10-26 14:21:20 -0400

    Working with initial dataset ish

---
+++ src/data_preprocessing/preprocess_data.py
@@ -0,0 +1,103 @@
+'''
+I made this a separate file because we are probably going to use it in other parts of our project
+'''
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+
+def read_data(data):
+    '''
+    Will return arrays of these sizes:
+        X_train : (752494, 42)
+        y_train_label : (752494, )
+        y_train_binary : ()
+
+        X_test : (82333, 42)
+        y_test_label : (82333, )
+        y_train_binary : (82333, )
+    '''
+
+    # convert all but last column to list of lists for data
+    X = np.array(data.iloc[:,:-2].values.tolist())
+    X = StandardScaler().fit_transform(X)  # mean of ~0 and variance of 1
+    # convert last column to list for labels
+    y_binary = np.array(data.iloc[:,-1].values.tolist())
+    y_labels = np.array(data.iloc[:,-2].values.tolist())
+    return X, y_binary,y_labels
+
+# only call this on training set
+def PCA_train_test(X, y):
+    # pca is NOT optional
+    X, pca = pca_data(X)
+    desc = '_PCA'
+
+    np.save("X_train%s.npy" % desc, X)
+    np.save("y_train%s.npy" % desc, y)
+    return X, y, pca
+
+def pca_data(X):
+    pca = PCA()
+    X = pca.fit_transform(X)
+
+    #get variance explained
+    explained_variance = pca.explained_variance_ratio_
+    '''
+    #make first plot of just principal components
+    fig1 = plt.figure()
+    plt.plot(explained_variance)
+    plt.title("Principal Components")
+    plt.ylabel("Percent of Variance Explained")
+    plt.xlabel("Principal Component")
+    plt.savefig("graphs/principal_comp_.png")
+    '''
+    #select what percent var to keep
+    desired_var = 0.9
+    #select how many eigenvalues to keep
+    cumsum = np.cumsum(explained_variance)
+    k = np.argwhere(cumsum > desired_var)[0]
+
+    '''
+    #make second plot of cum var explained
+    fig2 = plt.figure()
+    plt.plot(cumsum)
+    plt.title("Variance Explained")
+    plt.plot(k, cumsum[k], 'ro', label="Eigenvalue #%d with %.2f Variance" % (k, desired_var))
+    plt.legend()
+    plt.ylabel("Cumulative Percent of Variance Explained")
+    plt.xlabel("Principal Component")
+    plt.savefig("graphs/var_exp_.png")
+    '''
+    pca = PCA(n_components=int(k))
+    X = pca.fit_transform(X)
+
+    return X, pca
+            
+
+def main():
+
+    # training data
+    data = pd.read_csv('../../data/UNSW_NB15_training-set_original.csv')
+    X, y_bin, y_lab = read_data(data)
+    print(x[0:5])
+    print(y_bin[0:5])
+    print(y_lab[0:5])
+    #X, y, pca = PCA_train_test(X, y)
+    ''' 
+    # testing data
+    data = pd.read_csv("../../data/UNSW_NB15_testing-set.csv")
+    X, y = read_data(data)
+    X_test_pca = pca.fit_transform(X)
+    np.save("X_test_PCA.npy", X_test_pca)
+    np.save("y_test_PCA.npy", y)
+    '''
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ src/data_preprocessing/remove_duplicates.py
@@ -0,0 +1,76 @@
+import csv
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from operator import itemgetter
+
+# helper function to cast items to float where ever possible
+def float_cast(item):
+    try:
+        return float(item)
+    except ValueError:
+        return item
+
+def count_plot(labels):
+    label_dict = {}
+    for l in labels:
+        # if l == 'normal' or l == 'neptune':
+        #    continue
+        if l not in label_dict:
+            label_dict[l] = 1
+        else:
+            label_dict[l] += 1
+    label_dict = OrderedDict(sorted(label_dict.items(), key=itemgetter(1), reverse=True))
+    
+    fig = plt.figure(figsize=(100, 100))
+    plt.bar(label_dict.keys(), label_dict.values())
+    plt.xticks(rotation=90, fontsize=100)
+    plt.yticks(fontsize=15)
+    # plt.title("Distribution of Attacks Without Neptune")
+    # plt.savefig("../graphs/attacks_without_neptune.png")
+    # plt.title("Distribution of Attacks Only")
+    # plt.savefig("../graphs/attacks_only.png")
+    plt.title("Distribution of Samples", fontsize=15)
+    plt.savefig("../graphs/label_distribution_train.png", bbox='tight_layout')
+    
+
+def main():
+
+    # converts every row in the train data to a string, then adds it to a set
+    # if the same string row already exists in the set, then it will not be added
+    f=open("../../data/UNSW_NB15_training-set_original.csv","r")
+    line_set={"0"}
+    while True:
+        line=f.readline()
+        if line =="":
+            break
+        if "attack_cat" in line:
+            continue
+        line=line[:-1]  #YO I THINK THIS SHOULD BE -1 NOT -2!!*******
+        line_set.add(line)
+    line_set.remove("0")
+    
+    # goes through the unduplicated string set now and splits the data by comma
+    # converts to float where possible
+    list_data = []
+    for line in line_set:
+        line = line.split(',')
+        line = [float_cast(l) for l in line]
+        list_data.append(line)
+
+    # changes all of the categorical variables into ordinally encoded variables
+    # saves this result into csv file to be used for predictions
+    list_data = pd.DataFrame(list_data)
+    list_data = list_data.iloc[:,1:]
+
+    #count_plot(list_data.iloc[:,-2].values.tolist()) #MAKE LABELS BIGGER!! TODO
+
+    enc = OrdinalEncoder()
+    list_data.iloc[:,1:4] = enc.fit_transform(list_data.iloc[:,1:4])
+    
+    list_data.to_csv("../../data/UNSW_train.csv",index=False)  
+
+if __name__ == "__main__":
+    exit(main())
