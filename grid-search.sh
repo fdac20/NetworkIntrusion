2c98cb17da8a636ab9a556a2dfba9d3b6bc9ad3b
Charlie <chazzo96@yahoo.com>
2020-10-30 15:25:11 -0400

    Adding script for run on the cluster

---
+++ grid-search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=finalProject
+#SBATCH --output=finalProject_%A_%a.out
+#SBATCH --error=finalProject_%A_%a.err
+#SBATCH --array=1-20
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --exclusive
+
+# Print the task id.
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+# Add lines here to run your computations.
+python random_search.py $SLURM_ARRAY_TASK_ID
